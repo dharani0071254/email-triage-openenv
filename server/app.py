544f951060a177c7bc6af78381da03f0a34d4e43
Dharani Dharan <dharani2rich@gmail.com>
2026-04-08 23:10:44 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -20,3 +20,10 @@ def step(action: dict):
         "reward": reward.value,
         "done": done
     }
+    def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
