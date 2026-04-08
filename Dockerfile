c1c7691c229b0d127cea6e4aa73bd38463fd75db
Dharani Dharan <dharani2rich@gmail.com>
2026-04-08 23:40:22 +0530

    commit

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["python", "server/app.py"]
+CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -41,7 +41,7 @@ def step(action: dict):
 # 🔥 ADD THIS PART (VERY IMPORTANT)
 def main():
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+    uvicorn.run("server:app", host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
